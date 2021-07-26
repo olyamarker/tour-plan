e5365a1b780eee6a44ffac134a0964fab30c638f
olyamarker <krasnova047@gmail.com>
2021-07-27 00:21:44 +0400

    create: mobile menu

--- js/main.js
+++ js/main.js
@@ -24,3 +24,11 @@ const reviewsSlider = new Swiper('.reviews-slider', {
     prevEl: '.reviews-slider__button--prev',
   },
 });
+
+var menuButton = document.querySelector('.menu-button');
+menuButton.addEventListener('click', function () {
+  console.log('Клик по кнопке Меню');
+  document
+  .querySelector('.navbar-bottom')
+  .classList.toggle('navbar-bottom--visible');
+});
