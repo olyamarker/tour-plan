aacf61a6d8e7b7ba6d93b5daa612f58f61556337
olyamarker <krasnova047@gmail.com>
2021-07-27 18:04:35 +0400

    added: modal block rating&feedback

--- footer.php
+++ footer.php
@@ -166,6 +166,7 @@
       </div>
       <!-- /.container -->
     </footer>
+    <script src="js/jquery-3.6.0.min.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
     <script src="js/parallax.min.js"></script>
     <script src="js/swiper-bundle.min.js"></script>
--- js/main.js
+++ js/main.js
@@ -1,34 +1,57 @@
-const hotelSlider = new Swiper('.hotel-slider', {
-  // Optional parameters
-  loop: true,
+$(document).ready(function (){
+  const hotelSlider = new Swiper('.hotel-slider', {
+    // Optional parameters
+    loop: true,
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.hotel-slider__button--next',
+      prevEl: '.hotel-slider__button--prev',
+    },
+    // Переключение слайдов с помощью клавиатуры
+    keyboard: {
+      enabled: true,
+      onlyInViewport: false,
+    },
+  });
+  
+  const reviewsSlider = new Swiper('.reviews-slider', {
+    // Optional parameterszx
+    loop: true,
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.reviews-slider__button--next',
+      prevEl: '.reviews-slider__button--prev',
+    },
+  });
+  
+  var menuButton = document.querySelector(".menu-button");
+  menuButton.addEventListener("click", function () {
+    console.log("Клик по кнопке Меню");
+    document
+    .querySelector(".navbar-bottom")
+    .classList.toggle("navbar-bottom--visible");
+  });
+  
+  var modalButton = $('[data-toggle=modal]');
+  var closeModalButton = $('.modal__close');
+  modalButton.on('click', openModal);
+  closeModalButton.on("click", closeModal)
 
-  // Navigation arrows
-  navigation: {
-    nextEl: '.hotel-slider__button--next',
-    prevEl: '.hotel-slider__button--prev',
-  },
-  // Переключение слайдов с помощью клавиатуры
-  keyboard: {
-    enabled: true,
-    onlyInViewport: false,
-  },
-});
+  function openModal() {
+    var targetModal = $(this).attr('data-href');
+    var modalOverlay = $('.modal__overlay');
+    var modalDialog = $('.modal__dialog');
+    $(targetModal).find('.modal__overlay').addClass('modal__overlay--visible');
+    $(targetModal).find('.modal__dialog').addClass('modal__dialog--visible');
+  }
 
-const reviewsSlider = new Swiper('.reviews-slider', {
-  // Optional parameterszx
-  loop: true,
-
-  // Navigation arrows
-  navigation: {
-    nextEl: '.reviews-slider__button--next',
-    prevEl: '.reviews-slider__button--prev',
-  },
-});
-
-var menuButton = document.querySelector('.menu-button');
-menuButton.addEventListener('click', function () {
-  console.log('Клик по кнопке Меню');
-  document
-  .querySelector('.navbar-bottom')
-  .classList.toggle('navbar-bottom--visible');
+  function closeModal(event) {
+    event.preventDefault();
+    var modalOverlay = $('.modal__overlay');
+    var modalDialog = $('.modal__dialog');
+    modalOverlay.removeClass('modal__overlay--visible');
+    modalDialog.removeClass('modal__dialog--visible');
+  }
 });
